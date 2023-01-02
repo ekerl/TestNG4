49e6ec9fdea6349489f80cbc2d73821315792464
PC <heysoff@outlook.com>
2023-01-02 19:17:04 +0200

    Initial commit

---
+++ src/Gun01/_01_Giris.java
@@ -0,0 +1,25 @@
+package Gun01;
+
+public class _01_Giris {
+
+    public static void main(String[]args){
+
+
+        void webSitesiniAc(){
+            System.out.println("Driver tanımlandı ve web sitesi açıldı....");
+        }
+
+        void LoginİsleminiYap(){
+            System.out.println("Driver tanımlandı ve web sitesi açıldı....");
+        }
+
+        @
+
+        void driveriKapat(){
+            System.out.println("Driver tanımlandı ve web sitesi açıldı....");
+        }
+
+
+
+
+}
